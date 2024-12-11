ff9cef5ffb8b29e9d9fc5a7067e14801272492b9
Alexander Seifert <alexander.seifert@amcon.de>
2024-12-11 12:25:52 +0100

    add file methods

---
+++ BuchhaltungSoftware/Models/fileMethods.py
@@ -0,0 +1,14 @@
+import os
+import string
+
+from PyQt6.QtWidgets import QApplication, QFileDialog
+app = QApplication([])
+fd = QFileDialog()
+
+def getFilepathAsString():
+    return fd.getOpenFileName()
+
+def loadFileFromFilepath(filepath: string):
+        if filepath[0] and os.path.isfile(filepath[0]):
+            with open(filepath[0], 'r') as file:
+                return file.read()
